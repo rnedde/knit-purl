02addb16a8a639d704b159af915e7cbfec629e67
rnedde <rnedde@gmail.com>
2025-06-11 15:53:21 +0800

    very basic ugly knit purl using vertices

--- public/script-tapestry.js
+++ public/script-tapestry.js
@@ -14,7 +14,9 @@ function setup() {
   canvas.style('z-index', '-1');
   noStroke();
   textSize(16);
+  // blendMode(DARKEST);
 
+  rectMode(CENTER);
   socket = io(); // Socket.IO client
 
   socket.on('fullMessage', (msg) => {
@@ -73,16 +75,17 @@ function drawSingleStitch(i) {
   let cols = floor(width / stitchSize);
   let row = floor(i / cols);
   let col = i % cols;
-  let y = row * stitchSize + 16;
-
   let x = (row % 2 === 0)
     ? col * stitchSize + stitchSize / 2
     : (cols - 1 - col) * stitchSize + stitchSize / 2;
 
-  let knitCol = colorsForStitches[i];
-  let stitchCol = binaryMessage[i] === '0'
-    ? knitCol
-    : darkenColor(knitCol, 0.7);
+  let y = row * stitchSize + stitchSize / 2;
+
+
+  let stitchCol = colorsForStitches[i];
+  // let stitchCol = binaryMessage[i] === '0'
+  // ? knitCol
+  // : darkenColor(knitCol, 0.7);
 
   if (binaryMessage[i] === '0') {
     drawKnit(x, y, stitchSize, stitchCol);
@@ -92,41 +95,132 @@ function drawSingleStitch(i) {
 }
 
 function drawKnit(x, y, size, color) {
+
+  // *** Option 2: ***
   let r = size / 2;
+
   push();
+  blendMode(DARKEST);
   fill(color);
   translate(x, y);
-  push();
-  translate(-5, 0);
-  rotate(PI / 3);
-  ellipse(0, 0, r * ellipseLength, r * ellipseWidth);
+  rect(0, 0, size);
   pop();
 
-  translate(5, 0);
-  rotate(-PI / 3);
-  ellipse(0, 0, r * ellipseLength, r * ellipseWidth);
+
+  push();
+
+  fill(color);
+  translate(x, y);
+  //create shadows
+  let shadow = 'rgba(0, 0, 0, 0.25)';
+  // fill(shadow);
+  stroke(0);
+  translate(0,-r);
+  //left knit
+  beginShape();
+  vertex(-r, -r);
+  vertex(0, 0);
+  vertex(0, 2 * r);
+  vertex(-r, r);
+  endShape(CLOSE);
+
+  //right knit
+
+  beginShape();
+  vertex(r, -r);
+  vertex(0, 0);
+  vertex(0, 2 * r);
+  vertex(r, r);
+  endShape(CLOSE);
+
+  
   pop();
-  pop();
+
+
+
+  // *** Option 1: *** 
+
+  // let r = size / 2;
+  // push();
+  // fill(color);
+  // translate(x, y);
+  // push();
+  // translate(-5, 0);
+  // rotate(PI / 3);
+  // ellipse(0, 0, r * ellipseLength, r * ellipseWidth);
+  // pop();
+
+  // translate(5, 0);
+  // rotate(-PI / 3);
+  // ellipse(0, 0, r * ellipseLength, r * ellipseWidth);
+  // pop();
+  // pop();
+
 }
+
+
+
 
 function drawPurl(x, y, size, color) {
+
+  // *** Option 2: ***
+
   let r = size / 2;
+
   push();
+  blendMode(DARKEST);
   fill(color);
   translate(x, y);
+  rect(0, 0, size);
+  pop();
+
   push();
-  translate(-5, 0);
-  rotate(PI / 1.2);
-  ellipse(0, 0, r * ellipseLength / 1.1, r * ellipseWidth);
+  translate(x, y);
+  fill(color);
+  stroke(0);
+  //left purl
+  // beginShape();
+  // vertex(-r, 0);
+  // vertex(0, 0);
+  // vertex(-r/2, r);
+  // endShape(CLOSE);
+  // //right purl
+  // beginShape();
+  // vertex(0, 0);
+  // vertex(r, 0);
+  // vertex(r/2, r);
+  // endShape(CLOSE);
+  // middle purl
+
+  beginShape();
+  vertex(-r, -r/3);
+  vertex(0,-r);
+  vertex(r, -r/3);
+  vertex(r,0);
+  vertex(-r,0);
+  endShape(CLOSE);
+
+  
   pop();
 
-  translate(5, 0);
-  rotate(-PI / 1.2);
-  ellipse(0, 0, r * ellipseLength / 1.1, r * ellipseWidth);
-  pop();
-  pop();
+
+  // *** Option 1: ***
+  // let r = size / 2;
+  // push();
+  // fill(color);
+  // translate(x, y);
+  // push();
+  // translate(-5, 0);
+  // rotate(PI / 1.2);
+  // ellipse(0, 0, r * ellipseLength / 1.1, r * ellipseWidth);
+  // pop();
+
+  // translate(5, 0);
+  // rotate(-PI / 1.2);
+  // ellipse(0, 0, r * ellipseLength / 1.1, r * ellipseWidth);
+  // pop();
+  // pop();
 }
-
 
 function autoScrollIfNeeded() {
   // Find the bottom of the last drawn stitch
@@ -142,4 +236,8 @@ function autoScrollIfNeeded() {
     // Scroll smoothly down a bit
     window.scrollBy({ top: stitchSize, behavior: 'smooth' });
   }
+}
+
+function mousePressed() {
+  console.log("clicked at:", mouseX, mouseY);
 }
